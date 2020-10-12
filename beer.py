0fcb09dc341ed557bbad3d914b7e5d4c064af630
uttkarsh21 <narayan.u@husky.neu.edu>
2020-10-11 21:43:39 -0400

    beer

--- beer.py
+++ beer.py
@@ -12,13 +12,6 @@ def job():
 
 schedule.every().day.at("18:33").do(job)
 
-Oclock = sa.WaveObject.from_wave_file("BeerOclock.wav")
-
-for x in range(3):
-        print("beerOclock %d" % (x))
-        play_obj = Oclock.play()
-        play_obj.wait_done()
-        
 while 1:
     schedule.run_pending()
     time.sleep(1)
