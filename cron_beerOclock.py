76ae2ac0ffd1834985bb17e8362abc6bcfb7dea3
uttka <uttkarsh21@gmail.com>
2020-10-13 16:56:36 -0400

    cron

--- cron_beerOclock.py
+++ cron_beerOclock.py
@@ -2,6 +2,8 @@ import schedule
 import time
 import simpleaudio as sa
 
+#crontab enter 33 18 * * * /home/uttkarsh/cron_beerOclock.py
+
 Oclock = sa.WaveObject.from_wave_file("BeerOclock.wav")
 
 for x in range(3):
