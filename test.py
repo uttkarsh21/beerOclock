2ca4ad5be8db50fd7b767ff2ed6996682c7ba2bf
uttka <uttkarsh21@gmail.com>
2020-10-13 01:31:23 -0400

    test+

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import schedule
 import time
 import simpleaudio as sa
 
+Oclock = sa.WaveObject.from_wave_file("BeerOclock.wav")
+
 for x in range(3):
         print("beerOclock %d" % (x))
         play_obj = Oclock.play()
