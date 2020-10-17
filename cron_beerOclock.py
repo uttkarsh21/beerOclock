3ae6b54c68908136ea41c8537c77ac712ae2d19c
uttkarsh21 <narayan.u@husky.neu.edu>
2020-10-17 18:05:25 -0400

    cron stuff

--- cron_beerOclock.py
+++ cron_beerOclock.py
@@ -1,12 +1,12 @@
+#!/usr/bin/python3
+
 import schedule
 import time
 import simpleaudio as sa
 
-#crontab enter 33 18 * * * /home/uttkarsh/cron_beerOclock.py
-
 Oclock = sa.WaveObject.from_wave_file("BeerOclock.wav")
 
-for x in range(3):
-        print("beerOclock %d" % (x))
-        play_obj = Oclock.play()
-        play_obj.wait_done()
+#for x in range(3):
+#print("beerOclock %d" % (x))
+play_obj = Oclock.play()
+play_obj.wait_done()
